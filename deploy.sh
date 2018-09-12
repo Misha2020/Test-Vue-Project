c3453f1ea054e976bb462a908f90d7af1c285c69
Misha <bondarchuk4321@gmail.com>
2018-09-12 10:58:25 +0300

    deploy

--- deploy.sh
+++ deploy.sh
@@ -16,7 +16,8 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:Misha2020/Misha2020.github.io.git master
+# https://github.com/MykhayloBondacrhuk/Test-Vue-Project.git
+git push -f https://github.com/MykhayloBondacrhuk.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
